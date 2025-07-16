4c054421731bbae9d9824d8ccc1c2ddb803a8e71
vimal28346 <vimalkrishna14@gmail.com>
2025-07-16 22:56:21 +0530

    Added some lines

--- eg.java
+++ eg.java
@@ -1,5 +1,12 @@
+import java.util.*;
 class Main{
     public static void main(String x[]){
-        System.out.println("Learning Java is fun!");
+        System.out.println("Enter two numbers:");
+        Scanner obj = new Scanner(System.in);
+        int a = obj.nextInt();
+        int b = obj.nextInt();
+        int sum = a + b;
+        System.out.println("The sum is: " + sum);
+        obj.close();
     }
 }
